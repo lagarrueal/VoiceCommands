452a1aac2594a47eaa9c2947bde2d67523ccf42e
Alexandre <lagarrueal@cy-tech.fr>
2023-01-24 19:21:06 +0100

    Changes on demokit (useless until we don't have a working model)

--- demo_kit/demonstration.py
+++ demo_kit/demonstration.py
@@ -1,9 +1,13 @@
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
+import os
 
-dir = 'project/VoiceCommands/demo_kit/images/'
-img_path = dir + 'arrow_left.png'
+dir = 'VoiceCommands/demo_kit/images/'
+img_path = dir + 'left.png'
 print(img_path)
+
+
+print(os.listdir(dir))
 
 # plot an image from the directory dir
 img = mpimg.imread(img_path)
